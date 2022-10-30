cc9c1cd6bab479704d02dc7522e6630a6902d02a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-10-30 18:22:34 +0800

    add test cases

---
+++ src/use-event.test.ts
@@ -0,0 +1,13 @@
+import defaultUseEvent, { useEvent, dispatch } from './use-event'
+
+describe('export functions', () => {
+  it('should export useEvent function as default export', () => {
+    expect(defaultUseEvent).toBe(useEvent)
+  })
+  it('should export useEvent function', () => {
+    expect(typeof useEvent).toBe('function')
+  })
+  it('should export dispatch function', () => {
+    expect(typeof dispatch).toBe('function')
+  })
+})
