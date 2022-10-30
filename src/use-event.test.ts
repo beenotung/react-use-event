36ffd3d8124e370bbdda1839320174502f69443a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-10-30 18:23:54 +0800

    lint fix

--- src/use-event.test.ts
+++ src/use-event.test.ts
@@ -1,13 +1,13 @@
-import defaultUseEvent, { useEvent, dispatch } from './use-event'
+import defaultUseEvent, { useEvent, dispatch } from './use-event';
 
 describe('export functions', () => {
   it('should export useEvent function as default export', () => {
-    expect(defaultUseEvent).toBe(useEvent)
-  })
+    expect(defaultUseEvent).toBe(useEvent);
+  });
   it('should export useEvent function', () => {
-    expect(typeof useEvent).toBe('function')
-  })
+    expect(typeof useEvent).toBe('function');
+  });
   it('should export dispatch function', () => {
-    expect(typeof dispatch).toBe('function')
-  })
-})
+    expect(typeof dispatch).toBe('function');
+  });
+});
